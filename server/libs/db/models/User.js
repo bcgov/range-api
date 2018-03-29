1727e43462461e367773e9b28575834f5ff6b448
Jason C. Leach <jason.leach@fullboar.ca>
2018-03-29 16:44:52 -0700

    Initial work on User model

---
+++ server/libs/db/models/User.js
@@ -0,0 +1,68 @@
+//
+// MyRA
+//
+// Copyright © 2018 Province of British Columbia
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Created by Jason Leach on 2018-02-21.
+//
+
+/* eslint-env es6 */
+
+'use strict';
+
+export default (sequelize, DataTypes) => {
+  const User = sequelize.define('user', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    username: {
+      allowNull: false,
+      type: DataTypes.STRING(16),
+      unique: true,
+    },
+    givenName: {
+      type: DataTypes.STRING(32),
+    },
+    familyName: {
+      type: DataTypes.STRING(32),
+    },
+    email: {
+      allowNull: false,
+      unique: true,
+      type: DataTypes.STRING(32),
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP(3)'),
+      allowNull: false,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP(3)'),
+      allowNull: false,
+    },
+  }, {
+    freezeTableName: true,
+    timestamps: false,
+    underscored: true,
+    tableName: 'user_account',
+  });
+
+  return User;
+};
+
