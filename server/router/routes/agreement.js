8ecee0fe9925442de06ec25ecd1269d069b4cf37
Kyubin <kyubin@freshworks.io>
2018-03-29 19:46:10 -0700

    initial pagination setup for agreements

--- server/router/routes/agreement.js
+++ server/router/routes/agreement.js
@@ -208,31 +208,39 @@ const transformAgreement = (agreement, clientTypes) => {
 
 // Get all agreements
 router.get('/', asyncMiddleware(async (req, res) => {
-  const { term } = req.query;
-
+  const { term = '', page = 1 } = req.query;
+  const limit = 10;
+  const offset = limit * (page - 1);
+  const where = {
+    [Op.or]: [
+      {
+        id: {
+          [Op.iLike]: `%${term}%`, // (iLike: case insensitive)
+        },
+      },
+    ],
+  };
   try {
     const clientTypes = await ClientType.findAll();
-    const results = await Agreement.findAll({
-      limit: 10,
+    const agreements = await Agreement.findAll({
+      limit,
+      offset,
       include: allAgreementChildren,
       attributes: {
         exclude: excludedAgreementAttributes,
       },
-      where: {
-        [Op.or]: [
-          {
-            id: {
-              [Op.iLike]: `%${term || ''}%`, // (iLike: case insensitive)
-            },
-          },
-        ],
-      },
+      where,
     });
-
     // apply and transforms to the data structure.
-    const agreements = results.map(result => transformAgreement(result, clientTypes));
+    const transformedAgreements = agreements.map(result => transformAgreement(result, clientTypes));
+    const result = {
+      perPage: limit,
+      currentPage: page,
+      total: await Agreement.count({ where }),
+      agreements: transformedAgreements,
+    };
 
-    res.status(200).json(agreements).end();
+    res.status(200).json(result).end();
   } catch (err) {
     throw err;
   }
