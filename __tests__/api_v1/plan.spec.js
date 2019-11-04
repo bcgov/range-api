56fc303e46f65a96d6f099d9bb0d49875c6090cd
Caleb Sharp <epicsharp11@gmail.com>
2019-11-04 14:54:05 -0800

    Check for canonical ids in plan response

--- __tests__/api_v1/plan.spec.js
+++ __tests__/api_v1/plan.spec.js
@@ -6,6 +6,20 @@ import zoneMocks from '../../__mocks__/fixtures/ref_zone_mock.json';
 import agreementMocks from '../../__mocks__/fixtures/agreement_mock.json';
 import planMocks from '../../__mocks__/fixtures/plan_mock.json';
 import planVersionMocks from '../../__mocks__/fixtures/plan_version_mock.json';
+import pastureMocks from '../../__mocks__/fixtures/pasture_mock.json';
+import plantCommunityMocks from '../../__mocks__/fixtures/plant_community_mock.json';
+import plantCommunityActionMocks from '../../__mocks__/fixtures/plant_community_action_mock.json';
+import indicatorPlantMocks from '../../__mocks__/fixtures/indicator_plant_mock.json';
+import monitoringAreaMocks from '../../__mocks__/fixtures/monitoring_area_mock.json';
+import monitoringAreaPurposeMocks from '../../__mocks__/fixtures/monitoring_area_purpose_mock.json';
+import grazingScheduleMocks from '../../__mocks__/fixtures/schedule_mock.json';
+import grazingScheduleEntryMocks from '../../__mocks__/fixtures/schedule_entry_mock.json';
+import additionalRequirementMocks from '../../__mocks__/fixtures/additional_requirement_mock.json';
+import ministerIssueMocks from '../../__mocks__/fixtures/minister_issue_mock.json';
+import ministerIssueActionMocks from '../../__mocks__/fixtures/minister_issue_action_mock.json';
+import ministerIssuePastureMocks from '../../__mocks__/fixtures/minister_issue_pasture_mock.json';
+import managementConsiderationMocks from '../../__mocks__/fixtures/management_consideration_mock.json';
+import invasivePlantMocks from '../../__mocks__/fixtures/invasive_plant_mock.json';
 import clientAgreementMocks from '../../__mocks__/fixtures/client_agreement_mock.json';
 import planConfirmationMocks from '../../__mocks__/fixtures/plan_confirmation_mock.json';
 import DataManager from '../../src/libs/db2';
@@ -15,6 +29,15 @@ const dm = new DataManager(config);
 
 jest.mock('request-promise-native');
 
+const hasNested = (object, key, i = 0) => {
+  const keys = Object.keys(object);
+
+  if (Object.prototype.hasOwnProperty.call(object, key)) return true;
+  if (object && typeof object === 'object' && keys.length > 0) {
+    return Object.keys(object).some(k => object[k] && hasNested(object[k], key, i + 1));
+  }
+  return false;
+};
 
 const { canAccessAgreement } = passport.aUser;
 const truncate = table => `TRUNCATE TABLE ${table} RESTART IDENTITY CASCADE`;
@@ -37,8 +60,22 @@ const truncateTables = async () => {
   await dm.db.schema.raw(truncate('plan_confirmation'));
   await dm.db.schema.raw(truncate('client_agreement'));
   await dm.db.schema.raw(truncate('agreement'));
-  await dm.db.schema.raw(truncate('plan_version'));
+  await dm.db.schema.raw(truncate('management_consideration'));
+  await dm.db.schema.raw(truncate('minister_issue_pasture'));
+  await dm.db.schema.raw(truncate('minister_issue_action'));
+  await dm.db.schema.raw(truncate('minister_issue'));
+  await dm.db.schema.raw(truncate('additional_requirement'));
+  await dm.db.schema.raw(truncate('grazing_schedule_entry'));
+  await dm.db.schema.raw(truncate('grazing_schedule'));
+  await dm.db.schema.raw(truncate('monitoring_area_purpose'));
+  await dm.db.schema.raw(truncate('monitoring_area'));
+  await dm.db.schema.raw(truncate('indicator_plant'));
+  await dm.db.schema.raw(truncate('plant_community'));
+  await dm.db.schema.raw(truncate('plant_community_action'));
+  await dm.db.schema.raw(truncate('pasture'));
+  await dm.db.schema.raw(truncate('invasive_plant_checklist'));
   await dm.db.schema.raw(truncate('plan'));
+  await dm.db.schema.raw(truncate('plan_version'));
 };
 
 describe('Test Plan routes', () => {
@@ -56,6 +93,19 @@ describe('Test Plan routes', () => {
     const user = userMocks[0];
     const zone = zoneMocks[0];
     const agreement = agreementMocks[0];
+    const pasture = pastureMocks[0];
+    const plantCommunity = plantCommunityMocks[0];
+    const plantCommunityAction = plantCommunityActionMocks[0];
+    const indicatorPlant = indicatorPlantMocks[0];
+    const monitoringArea = monitoringAreaMocks[0];
+    const monitoringAreaPurpose = monitoringAreaPurposeMocks[0];
+    const grazingSchedule = grazingScheduleMocks[0];
+    const grazingScheduleEntry = grazingScheduleEntryMocks[0];
+    const additionalRequirement = additionalRequirementMocks[0];
+    const ministerIssue = ministerIssueMocks[0];
+    const ministerIssueAction = ministerIssueActionMocks[0];
+    const ministerIssuePasture = ministerIssuePastureMocks[0];
+    const managementConsideration = managementConsiderationMocks[0];
     const clientAgreement = clientAgreementMocks[0];
     const planConfirmation = planConfirmationMocks[0];
     await dm.db('user_account').insert([user]);
@@ -63,8 +113,22 @@ describe('Test Plan routes', () => {
     await dm.db('agreement').insert([agreement]);
     await dm.db('client_agreement').insert([clientAgreement]);
     await dm.db('plan').insert(planMocks);
-    await dm.db('plan_confirmation').insert([planConfirmation]);
     await dm.db('plan_version').insert(planVersionMocks);
+    await dm.db('plan_confirmation').insert([planConfirmation]);
+    await dm.db('pasture').insert([pasture]);
+    await dm.db('plant_community').insert([plantCommunity]);
+    await dm.db('plant_community_action').insert([plantCommunityAction]);
+    await dm.db('indicator_plant').insert([indicatorPlant]);
+    await dm.db('monitoring_area').insert([monitoringArea]);
+    await dm.db('monitoring_area_purpose').insert([monitoringAreaPurpose]);
+    await dm.db('grazing_schedule').insert([grazingSchedule]);
+    await dm.db('grazing_schedule_entry').insert([grazingScheduleEntry]);
+    await dm.db('additional_requirement').insert([additionalRequirement]);
+    await dm.db('minister_issue').insert([ministerIssue]);
+    await dm.db('minister_issue_action').insert([ministerIssueAction]);
+    await dm.db('minister_issue_pasture').insert([ministerIssuePasture]);
+    await dm.db('management_consideration').insert([managementConsideration]);
+    await dm.db('invasive_plant_checklist').insert(invasivePlantMocks);
   });
 
   afterEach(async () => {
@@ -81,6 +145,17 @@ describe('Test Plan routes', () => {
       .expect('Content-Type', /json/)
       .expect((res) => {
         expect(res.body.id).toEqual(1);
+      });
+  });
+
+  test.only("Fetching a plan doesn't include any canonical ids in the responses", async () => {
+    await request(app)
+      .get(`${baseUrl}/1`)
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .expect((res) => {
+        expect(res.body.id).toEqual(1);
+        expect(hasNested(res.body, 'canonicalId')).not.toEqual(true);
       });
   });
 
