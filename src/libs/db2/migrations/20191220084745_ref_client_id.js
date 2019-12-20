5eb021247a4ff103bbee7b1587b5883cc1b309e6
Caleb Sharp <caleb.sharp@twostoryrobot.com>
2019-12-20 13:08:49 -0800

    Add new id column to ref_client

---
+++ src/libs/db2/migrations/20191220084745_ref_client_id.js
@@ -0,0 +1,32 @@
+
+exports.up = async (knex) => {
+  // Change primary key of ref_client to `id`
+  await knex.raw(`
+    ALTER TABLE ref_client 
+      DROP CONSTRAINT ref_client_pkey CASCADE,
+      ADD COLUMN id SERIAL,
+      ADD PRIMARY KEY (id);
+  `);
+
+  // Update foreign key references in other tables
+  await knex.raw(`
+    ALTER TABLE client_agreement 
+      DROP COLUMN client_id,
+      ADD COLUMN client_id INTEGER NOT NULL,
+      ADD FOREIGN KEY (client_id) REFERENCES ref_client(id);
+  `);
+
+  await knex.raw(`
+    ALTER TABLE plan_confirmation
+      DROP COLUMN client_id,
+      ADD COLUMN client_id INTEGER NOT NULL,
+      ADD FOREIGN KEY (client_id) REFERENCES ref_client(id);
+  `);
+
+  await knex.raw(`
+    ALTER TABLE user_account
+      DROP COLUMN client_id,
+      ADD COLUMN client_id INTEGER,
+      ADD FOREIGN KEY (client_id) REFERENCES ref_client(id);
+  `);
+};
