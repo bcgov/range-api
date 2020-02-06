44556728cc23f11d1cc9359251c1ead8e23968d6
Caleb Sharp <caleb.sharp@twostoryrobot.com>
2020-02-06 13:21:02 -0800

    Add plansnapshot model

---
+++ src/libs/db2/model/plansnapshot.js
@@ -0,0 +1,28 @@
+import Model from './model';
+import User from './user';
+
+export default class PlanSnapshot extends Model {
+  constructor(data, db = undefined) {
+    const obj = {};
+    Object.keys(data).forEach((key) => {
+      if (PlanSnapshot.fields.indexOf(`${PlanSnapshot.table}.${key}`) > -1) {
+        obj[key] = data[key];
+      }
+    });
+
+    super(obj, db);
+
+    this.creator = new User(User.extract(data));
+  }
+
+  static get fields() {
+    // primary key *must* be first!
+    return [
+      'id', 'snapshot', 'plan_id', 'created_at',
+    ].map(f => `${PlanSnapshot.table}.${f}`);
+  }
+
+  static get table() {
+    return 'plan_version';
+  }
+}
