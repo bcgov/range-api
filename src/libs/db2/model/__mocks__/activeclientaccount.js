76aec9078a28ef1b40ce467df0b9c5820bb24ebb
Caleb Sharp <caleb.sharp@twostoryrobot.com>
2020-04-03 12:37:17 -0700

    Mock active client account model

---
+++ src/libs/db2/model/__mocks__/activeclientaccount.js
@@ -0,0 +1,23 @@
+import assert from 'assert';
+import Model from '../model';
+import fixtures from '../../../../../__mocks__/fixtures';
+
+export default class ActiveClientAccount extends Model {
+  static findWithExclusion() {
+    return fixtures.allUsers;
+  }
+
+  static create(db, values) {
+    try {
+      assert(db);
+      assert(values);
+
+      if ((values.user_id === 1 || values.user_id === '1') && values.client_id) {
+        return { ...values, id: 1, clientId: values.client_id };
+      }
+      throw Error('Wrong user');
+    } catch (err) {
+      throw Error('Unexpected input or wrong user');
+    }
+  }
+}
