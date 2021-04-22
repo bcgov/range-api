080c647059801df248e73f0d049f5d200afd51b0
Robert Johnstone <rob@plasticviking.com>
2021-04-22 16:36:44 +0800

    wip

---
+++ .pipeline/lib/build.js
@@ -0,0 +1,24 @@
+'use strict';
+const {OpenShiftClientX} = require('pipeline-cli')
+const path = require('path');
+
+module.exports = (settings) => {
+  const phases = settings.phases
+  const options = settings.options
+  const oc = new OpenShiftClientX(Object.assign({'namespace':phases.build.namespace}, options));
+  const phase='build'
+  let objects = []
+  const templatesLocalBaseUrl =oc.toFileUrl(path.resolve(__dirname, '../../openshift'))
+
+  // The building of your cool app goes here ▼▼▼
+  objects.push(...oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/app.bc.yaml`, {
+    'param':{
+      'NAME': phases[phase].name,
+      'GIT_REPO_URL': oc.git.http_url,
+      'GIT_REF': 'master'
+    }
+  }));
+
+  console.log(`${JSON.stringify(objects, null, 2)}`);
+  oc.applyAndBuild(objects)
+}
