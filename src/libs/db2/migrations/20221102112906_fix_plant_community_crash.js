4a7161f4aaa34f21c7c81ca065d4676c3201f45b
Mike Wells <mw@air.local>
2022-11-02 11:33:31 -0700

    dont let null plant community or action names crash plan page

---
+++ src/libs/db2/migrations/20221102112906_fix_plant_community_crash.js
@@ -0,0 +1,45 @@
+
+exports.up = function(knex) {
+  await knex.raw(`
+CREATE OR REPLACE FUNCTION plant_community_no_null_name()
+RETURNS trigger AS
+$BODY$
+BEGIN
+  IF NEW.name IS NULL THEN
+    NEW.name = '';
+  END IF;
+  RETURN NEW;
+END
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION plant_community_action_no_null_name()
+RETURNS trigger AS
+$BODY$
+BEGIN
+  IF NEW.name IS NULL THEN
+    NEW.name = '';
+  END IF;
+  RETURN NEW;
+END
+$BODY$
+LANGUAGE plpgsql;
+
+drop trigger if exists plant_community_name on plant_community;
+drop trigger if exists plant_community_action_name on plant_community_action;
+
+CREATE TRIGGER plant_community_name  BEFORE  insert OR update
+    ON plant_community
+    For each row
+    EXECUTE FUNCTION plant_community_no_null_name();
+
+CREATE TRIGGER plant_community_action_name  BEFORE  insert OR update 
+    ON plant_community_action
+    EXECUTE FUNCTION plant_community_action_no_null_name();
+`);
+  
+};
+
+exports.down = function(knex) {
+  
+};
