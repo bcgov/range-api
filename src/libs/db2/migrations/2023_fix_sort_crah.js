b8626e9705c10db097ddf820a4681c85e05576f0
Micheal Wells <micheal.w.wells@gmail.com>
2023-02-02 09:34:20 -0800

    quick fix to add dummy column to avoid crash, column not used or is computed client side

---
+++ src/libs/db2/migrations/2023_fix_sort_crah.js
@@ -0,0 +1,34 @@
+//
+// MyRA
+//
+// Copyright © 2018 Province of British Columbia
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Created by Jason Leach on 2018-05-23.
+//
+
+'use strict';
+
+/* eslint-disable no-unused-vars */
+
+exports.up = (knex) => {
+  const query = `
+  alter table grazing_schedule_entry add column if not exists crown_au_ms integer default 0;
+  `;
+
+  return knex.schema.raw(query);
+};
+
+exports.down = (knex) => {
+};
