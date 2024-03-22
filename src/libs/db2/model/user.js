71f0f7eb765f9008704fdca16158286f96b8e56e
Brijesh <brijesh.amin@gov.bc.ca>
2024-03-22 13:11:37 -0700

    Angets too should receive plan status change emails

--- src/libs/db2/model/user.js
+++ src/libs/db2/model/user.js
@@ -20,8 +20,8 @@
 
 'use strict';
 
-import { SSO_ROLE_MAP } from "../../../constants";
-import Model from "./model";
+import { SSO_ROLE_MAP } from '../../../constants';
+import Model from './model';
 import UserClientLink from './userclientlink';
 
 export default class User extends Model {
@@ -44,25 +44,25 @@ export default class User extends Model {
       clientNumber: row.client_number,
       phoneNumber: row.phone_number,
       ssoId: row.sso_id,
-      roleId: row.role_id
+      roleId: row.role_id,
     };
   }
 
   static get fields() {
     // primary key *must* be first!
     return [
-      "id",
-      "username",
-      "given_name",
-      "family_name",
-      "email",
-      "phone_number",
-      "active",
-      "pia_seen",
-      "last_login_at",
-      "sso_id",
-      "role_id"
-    ].map(field => `${this.table}.${field}`);
+      'id',
+      'username',
+      'given_name',
+      'family_name',
+      'email',
+      'phone_number',
+      'active',
+      'pia_seen',
+      'last_login_at',
+      'sso_id',
+      'role_id',
+    ].map((field) => `${this.table}.${field}`);
   }
 
   static get table() {
@@ -75,38 +75,31 @@ export default class User extends Model {
       obj[Model.toSnakeCase(key)] = values[key];
     });
 
-    try {
-      const count = await db.table(User.table).where(where).update(obj);
+    const count = await db.table(User.table).where(where).update(obj);
 
-      if (count > 0) {
-        //   const res = await db.raw(`
-        //   SELECT user_account.*, ref_client.client_number FROM user_account
-        //   LEFT JOIN ref_client ON user_account.client_id = ref_client.id
-        //   WHERE user_account.id = ANY (?) ORDER BY ?;
-        // ` );
+    if (count > 0) {
+      //   const res = await db.raw(`
+      //   SELECT user_account.*, ref_client.client_number FROM user_account
+      //   LEFT JOIN ref_client ON user_account.client_id = ref_client.id
+      //   WHERE user_account.id = ANY (?) ORDER BY ?;
+      // ` );
 
-        // return res.rows.map(this.mapRow);
-        const [{ id }] = await db
-          .table(User.table)
-          .where(where)
-          .returning('id');
+      // return res.rows.map(this.mapRow);
+      const [{ id }] = await db.table(User.table).where(where).returning('id');
 
-        const res = await db.raw(
-          `
+      const res = await db.raw(
+        `
           SELECT user_account.*, ref_client.client_number FROM user_account
           LEFT JOIN user_client_link ON user_client_link.user_id = user_account.id
           LEFT JOIN ref_client ON ref_client.client_number = user_client_link.client_id
           WHERE user_account.id = ?;
         `,
-          [id],
-        );
-        return res.rows.map(User.mapRow)[0];
-      }
-
-      return [];
-    } catch (err) {
-      throw err;
+        [id],
+      );
+      return res.rows.map(User.mapRow)[0];
     }
+
+    return [];
   }
 
   static async create(db, values) {
@@ -115,40 +108,32 @@ export default class User extends Model {
       obj[Model.toSnakeCase(key)] = values[key];
     });
 
-    try {
-      const results = await db.table(User.table).returning('id').insert(obj);
+    const results = await db.table(User.table).returning('id').insert(obj);
 
-      return await User.findOne(db, { id: results.pop() });
-    } catch (err) {
-      throw err;
-    }
+    return await User.findOne(db, { id: results.pop() });
   }
 
   static async findWithExclusion(db, where, order, exclude) {
-    try {
-      const q = db.table(User.table).select('id').where(where);
+    const q = db.table(User.table).select('id').where(where);
 
-      if (exclude) {
-        q.andWhereNot(...exclude);
-      }
+    if (exclude) {
+      q.andWhereNot(...exclude);
+    }
 
-      const results = await q;
-      const userIds = results.map((obj) => obj.id);
+    const results = await q;
+    const userIds = results.map((obj) => obj.id);
 
-      const res = await db.raw(
-        `
+    const res = await db.raw(
+      `
         SELECT DISTINCT ON (user_account.id) user_id, user_account.*, ref_client.client_number FROM user_account
         LEFT JOIN user_client_link ON user_client_link.user_id = user_account.id
         LEFT JOIN ref_client ON ref_client.client_number = user_client_link.client_id
         WHERE user_account.id = ANY (?) ORDER BY user_account.id, ?;
       `,
-        [userIds, order],
-      );
+      [userIds, order],
+    );
 
-      return res.rows.map(User.mapRow);
-    } catch (err) {
-      throw err;
-    }
+    return res.rows.map(User.mapRow);
   }
 
   async getLinkedClientNumbers(db) {
@@ -170,6 +155,18 @@ export default class User extends Model {
       })
       .where({
         'user_client_link.client_id': clientId,
+      });
+    return result || [];
+  }
+
+  static async getAgentsFromAgreementId(db, agreementId) {
+    const result = await db
+      .table('user_account')
+      .join('client_agreement', {
+        'client_agreement.agent_id': 'user_account.id',
+      })
+      .where({
+        'client_agreement.agreement_id': agreementId,
       });
     return result || [];
   }
@@ -258,22 +255,22 @@ User.prototype.isRangeOfficer = function () {
   return this.roleId && this.roleId === 3;
 };
 
-User.prototype.isAgreementHolder = function() {
+User.prototype.isAgreementHolder = function () {
   return this.roleId && this.roleId === 4;
 };
 
-User.prototype.isReadOnly = function() {
+User.prototype.isReadOnly = function () {
   return this.roleId && this.roleId === 5;
 };
 
-User.prototype.canReadAll = function() {
-  return this.permissions && this.permissions.find(p => p.id === 1);
-}
+User.prototype.canReadAll = function () {
+  return this.permissions && this.permissions.find((p) => p.id === 1);
+};
 
-User.prototype.canReadZone = function() {
-  return this.permissions && this.permissions.find(p => p.id === 2);
-}
+User.prototype.canReadZone = function () {
+  return this.permissions && this.permissions.find((p) => p.id === 2);
+};
 
-User.prototype.canReadDistrict = function() {
-  return this.permissions && this.permissions.find(p => p.id === 3);
-}
+User.prototype.canReadDistrict = function () {
+  return this.permissions && this.permissions.find((p) => p.id === 3);
+};
